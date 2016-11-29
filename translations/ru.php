d179d10bf103dc32759b864de2ac15dd635c67fe
Alexey <none@none>
2016-11-29 21:29:21 +0700

    Добавлен первод для настроек

---
+++ translations/ru.php
@@ -0,0 +1,15 @@
+<?php
+
+	class imagePkgRuTranslation extends langPkgRuTranslation {
+	
+		public function getTranslations() {
+			return array(
+				'Images' => 'Изображения',
+				'Use EXIF orientation data for autorotation' => 'Использовать данные EXIF для автоповорота изображения'
+			);	
+		}
+		
+	}
+	
+	
+	
